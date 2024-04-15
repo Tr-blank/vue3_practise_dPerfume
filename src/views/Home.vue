347d837048e9b3571d28ee279f623790f0158574
Tr-blank <blank0129@gmail.com>
2024-04-16 02:08:45 +0800

    feat: add home content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,15 @@ import Newsletter from '@/components/Newsletter.vue'
 
 <template>
   <main>
+    <div class="main-container py-40">
+      <div class="max-w-[480px] mx-auto">
+        <h2 class="text-4xl pb-4">Perfume your day</h2>
+        <p class="text-2xl">
+          A perfume is like a piece of clothing, a message, a way of presenting oneself a costume
+          that according to the person who wears it.
+        </p>
+      </div>
+    </div>
     <Newsletter />
   </main>
 </template>
